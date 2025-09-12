5ea073ee9bcf08892b4f2edd66f073d61d3177a2
Md Aftab Uddin <aftabhasan7856@gmail.com>
2025-09-12 21:47:54 +0600

    all plant show in the main section

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 const categoryContainer = document.getElementById('category-container');
+ const middleTree = document.getElementById("middle-tree");
 
+const loadAllTree = ()=>{
+    fetch("https://openapi.programming-hero.com/api/plants")
+    .then(res => res.json())
+    .then(data =>{
+    displayPlant(data.plants)
+
+    })
+}
 const loadCategory = ()=>{
     fetch("https://openapi.programming-hero.com/api/categories")
     .then((res) => res.json())
@@ -14,26 +23,26 @@ showCategory(categories);
 loadCategory();
 const showCategory = (categories)=>{
  categoryContainer.innerHTML += `
-    <li data id="all" class="text-[gray]
+    <li data id="all" class="text-[gray] text-[12px]
      font-semibold bg-[#F0FDF4] hover:bg-[green] 
-    hover:text-[white] mb-1 p-2 rounded-lg cursor-pointer">
+    hover:text-[white] mb-1 p-2 rounded-lg cursor-pointer border-1">
       All
     </li>
   `;
     categories.forEach(cat => {
        
     categoryContainer.innerHTML += `<li onClick = "loadPlantByCat(${cat.id})"><button  class="  text-[gray]
-     font-semibold bg-[#F0FDF4] hover:bg-[green] 
-    hover:text-[white] mb-1 p-2 rounded-lg cursor-pointer">${cat.category_name}</button></li>`;
+     font-semibold bg-[#F0FDF4] hover:bg-[green] text-[12px]
+    hover:text-[white] mb-1 p-2 rounded-lg cursor-pointer border-1 w-full">${cat.category_name}</button></li>`;
 });
     categoryContainer.addEventListener('click',(e)=>{
         const allBtn = document.querySelectorAll("button")
         allBtn.forEach(button => {
-            button.classList.remove('bg-[green]')
+            button.classList.remove("bg-green-600", "text-white")
         });
         if(e.target.localName === 'button'){
            // console.log(e.target.id);
-            e.target.classList.add('bg-[green]')
+            e.target.classList.add("bg-green-600", "text-white")
            // loadPlantByCat(e.target.id)
         }
     });
@@ -54,21 +63,70 @@ const loadPlantByCat = (plantId)=>{
     })
 } 
 const displayPlant = (trees)=>{
-    const middleTree = document.getElementById("middle-tree");
+   
     middleTree.innerHTML = ''
     trees.forEach(tree =>{
-        console.log(tree);
+        //console.log(tree);
         const card = document.createElement("div")
-        card.innerHTML=`<div class="tree-card p-3 rounded-lg bg-[#ffffff] ">
-      <img src="${tree.image}" alt="" class="w-full h-[350px] object-cover mb-2 rounded-lg" />
+        card.innerHTML=`<div id="${tree.id}" class="tree-card p-4 rounded-lg bg-[#ffffff]  w-full shadow-lg">
+      <img src="${tree.image}" alt="" class="w-full h-[200px] object-cover mb-2 rounded-lg" />
       <h4 class="font-bold text-[1.5rem]">${tree.name}</h4>
-      <p>${tree.description}</p>
+      <p class = 'overflow-hidden  line-clamp-3'>${tree.description}</p>
       <div class="flex justify-between items-center my-2">
+        <div>
         <p class="text-[1rem] text-[#15803D] p-2 bg-[#DCFCE7] rounded-lg">${tree.category}</p>
+        </div>
+        <div>
         <p class="font-bold">৳<span>${tree.price}</span></p>
+        </div>
+
+        
       </div>
       <button class="rounded-[50px] bg-[#15803D] text-[#ffffff] p-2 cursor-pointer w-full">Add to Cart</button>
     </div>`
         middleTree.append(card)
     })
 }
+
+loadAllTree()
+const cartTree = document.getElementById("right-tree");
+let yourCarts = [];
+middleTree.addEventListener("click",(e)=>{
+    //console.log(e.target.innerText)
+    if(e.target.innerText === "Add to Cart"){
+        handleCarts(e)
+    }
+})
+const handleCarts = (e)=>{
+    //console.log("book click")
+
+        const cartTitle = e.target.parentNode.children[1].innerText;
+        //console.log(cartTitle)
+        const cartId = e.target.parentNode.id
+        //console.log(cartId)
+        const teePrice = e.target.parentNode.children[3].innerText
+        //console.log(teePrice)
+        yourCarts.push({
+            cartId :cartId ,
+            cartTitle:cartTitle,
+            teePrice:teePrice
+
+        })
+        showCartItem(yourCarts)
+}
+const showCartItem = (yourCarts)=>{
+    cartTree.innerHTML = "";
+    yourCarts.forEach(yourCart =>{
+        cartTree.innerHTML += `
+        <div class="flex justify-between mb-3 items-center bg-green-200">
+      <div>
+        <h2>${yourCart.cartTitle}</h2>
+        <p><span>${yourCart.teePrice}</span>* 1</p>
+      </div>
+      <p><i class="fa-solid fa-trash-can"></i></p>
+    </div>
+    
+        `
+    })
+}
+ 
